a68cc00104d0bb6a81e0a2fb3b8060c65ed5f68f
bcaron1000 <65052233+bcaron1000@users.noreply.github.com>
2021-10-27 16:19:20 -0600

    final commit3

--- src/js/components/cards/todoDirectory.js
+++ src/js/components/cards/todoDirectory.js
@@ -3,17 +3,12 @@ import makeElement from './../../utils/makeElement'
 const directory = function (){
     const template = `
     <aside id="directory" class="directory">
-    
     <ul id="todos" class="todoList">
-     
     </ul>
     <footer>
-
     </footer>
 </aside>
     `
-
     return makeElement(template)
 }
-
 export default directory
