9bfc88afe7d6de6c850afdc4ab8b1b7d6ce643e4
bcaron1000 <65052233+bcaron1000@users.noreply.github.com>
2021-10-28 17:05:46 -0600

    fourth commit

--- src/js/components/cards/todoitem.js
+++ src/js/components/cards/todoitem.js
@@ -1,46 +1,59 @@
+import { isDate } from "lodash";
 import makeElement from "../../utils/makeElement";
 
 
 const todoitem = function ({ id, category, title, endDate, isComplete}) {
   
-  
+  let itemCategory = category;
   let status = "";
-  let styling = ""
+  let styling = "";
+  let categoryTitle = "";
+
   if(isComplete === true ){
     status = "Completed"
   }else {
     status = "Incomplete"
   }
-let itemCategory = category;
+if (endDate > isDate){
+  console.log("true");
+}else{
+  console.log("False");
+}
 
 switch(itemCategory){
 
     case "work":
       styling = "work-style";
+      categoryTitle = "Work"
       break;
     case "school":
       styling = "school-style";
+      categoryTitle = "School "
       break;
     case "home":
       styling = "home-style";
+      categoryTitle = "Home"
       break;
     case "social":
       styling = "social-style";
+      categoryTitle = "Social"
       break;
     default:
       styling = "health-style";
+      categoryTitle = "Health"
 }
 
   const template = ` 
   <li class="card-style ${styling}" data-key="${id}">
     <div class="start-style">
-    <h2>${title} </h2>
+    <h2>${categoryTitle} </h2>
+    <h3>${title} </h3>
     <p>Due: ${endDate}</p>
     <p>${status}</p> 
     </div>
     <div class="end-style">
     
-    <p class="controls"><button>edit</button>
+    <p><button>edit</button>
     <button>delete</button></p> 
     </div>
     </li> 
